cda08b0059716abed1af707ed688b9e2ee9e2347
root <root@aike.brote.website>
2023-12-09 21:04:21 +0000

    New data

---
+++ go.mod
@@ -0,0 +1,10 @@
+module broteqr
+
+go 1.20
+
+require github.com/yougg/go-qrcode v0.0.0-20181009131600-c335135af91e
+
+require (
+	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646 // indirect
+	golang.org/x/image v0.0.0-20180926015637-991ec62608f3 // indirect
+)
---
+++ index2.go
@@ -0,0 +1,137 @@
+package main
+
+import (
+    "fmt"
+    "html/template"
+    "log"
+    "net/http"
+    qrcode "github.com/yougg/go-qrcode"
+)
+
+// Estructura para almacenar el historial de códigos QR
+var qrHistory []string
+
+func generateQRCode(w http.ResponseWriter, r *http.Request) {
+    // Obtener los datos del formulario
+    nombre := r.FormValue("nombre")
+    telefono := r.FormValue("telefono")
+    telefonolaboral := r.FormValue("telefonolaboral")
+    email := r.FormValue("email")
+    apellido := r.FormValue("apellido")
+    direccion := r.FormValue("direccion")
+    empresa := r.FormValue("empresa")
+    titulo := r.FormValue("titulo")
+    cargo := r.FormValue("cargo")
+    web := r.FormValue("web")
+    instagram := r.FormValue("instagram")
+    facebook := r.FormValue("facebook")
+    linkedin := r.FormValue("linkedin")
+    website := r.FormValue("website")
+    vcard := ""
+
+    if website != "" {
+        vcard += website
+    } else {
+        // Construir el contenido de la vCard
+        vcard += "BEGIN:VCARD\n" +
+            "VERSION:3.0\n" +
+            "N:" + nombre + ";" + apellido + ";;;\n" +
+            "TEL;TYPE=CELL:" + telefono + "\n"
+
+        if web != "" {
+            vcard += "URL:" + web + "\n"
+        }
+
+        if telefonolaboral != "" {
+            vcard += "TEL;TYPE=WORK:" + telefonolaboral + "\n"
+        }
+        if cargo != "" {
+            vcard += "ROLE:" + cargo + ";\n"
+        }
+
+        if empresa != "" {
+            vcard += "ORG:" + empresa + ";\n"
+        }
+
+        if email != "" {
+            vcard += "EMAIL:" + email + ";\n"
+        }
+
+        if direccion != "" {
+            vcard += "ADR:;;" + direccion + ";;;\n"
+        }
+
+        if titulo != "" {
+            vcard += "TITLE:" + titulo + ";\n"
+        }
+
+        if instagram != "" {
+            vcard += "X-SOCIALPROFILE;type=instagram:" + instagram + "\n"
+        }
+
+        if facebook != "" {
+            vcard += "X-SOCIALPROFILE;type=facebook:" + facebook + "\n"
+        }
+
+        if linkedin != "" {
+            vcard += "X-SOCIALPROFILE;type=linkedin:" + linkedin + "\n"
+        }
+
+        vcard += "END:VCARD"
+    }
+
+    fmt.Println(vcard)
+    var png []byte
+    png, err := qrcode.Encode(vcard, qrcode.Medium, 256, 256, 1)
+
+    // Obtener la imagen QR en bytes
+
+    // Establecer las cabeceras de respuesta HTTP
+    w.Header().Set("Content-Type", "image/png")
+
+    // Escribir los bytes de la imagen QR en el ResponseWriter
+    _, err = w.Write(png)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    // Guardar el código QR en el historial
+    qrHistory = append(qrHistory, vcard)
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+    if r.Method == "GET" {
+        // Mostrar el formulario HTML
+        tmpl := template.Must(template.ParseFiles("index2.html"))
+        err := tmpl.Execute(w, nil)
+        if err != nil {
+            log.Fatal(err)
+        }
+    } else if r.Method == "POST" {
+        // Procesar la solicitud POST
+        generateQRCode(w, r)
+    }
+}
+
+func qrHandler(w http.ResponseWriter, r *http.Request) {
+    generateQRCode(w, r)
+}
+
+func historyHandler(w http.ResponseWriter, r *http.Request) {
+    // Mostrar el historial de códigos QR generados
+    for _, qr := range qrHistory {
+        fmt.Fprintln(w, qr)
+    }
+}
+
+func main() {
+    http.HandleFunc("/", indexHandler)
+    http.HandleFunc("/qr", qrHandler)
+    http.HandleFunc("/history", historyHandler)
+
+    err := http.ListenAndServe(":7001", nil)
+    if err != nil {
+        fmt.Println(err)
+    }
+}
+
